9ec070ea30ba2f3f6e31a473422b9e82a9bf8d45
Yura <105108355+dizer2@users.noreply.github.com>
2022-08-01 09:01:32 +0200

    Add files via upload

---
+++ lesson 4 js-object/js/script.js
@@ -0,0 +1,127 @@
+// let computer = {
+// 	windows: 11,
+// 	name: 'asus',
+// 	processor: 'intel core i5',
+// 	videoCards: 'Gtx 3050ti',
+// 	operation: 16
+// }
+// let phone = {
+// 	name: 'SamsungA40',
+// 	color: 'blue',
+// 	camera: 1,
+// 	memory: 64,
+// 	bestProgram: 'Telegram',
+// 	chargingOutput: 'TPC',
+// 	cover: 'blue',
+// 	operationSystem: 'Android',
+// 	prise: 7000,
+// 	age: 4
+// }
+// let car = {
+// name: 'Ford',
+// speed: 200,
+// numberOfCities: 6,
+// fuel: 'gasoline',
+// prise: 200000,
+// theDriver: 'father',
+// color: 'gray',
+// tires: 'summer',
+// numberOfWheels: 4,
+// damage: 0,
+// tapeRecorder: 'Android',
+// salon: 'gray',
+// view: 'passenger car',
+// releaseAge: 2007,
+// minimumSpeed: 60,
+// spareWheels: 'yes',
+// spareFuel: 'yes',
+// scent: 'roses',
+// trunk: 'yes',
+// weight: 800
+// }
+// let favoritCat = {
+// color: 'gray',
+// dateBirthday: 2020,
+// weight: 10,
+// favoritFood: 'meat',
+// favoritGames: 'Catching up',
+// favoritDrink: 'Milk',
+// ogre: 'dog',
+// charakret: 'frendship',
+// married: 'false',
+// name: 'Myrchik',
+// middleName: 'Pushistik'
+// }
+// let workplace = {
+// color: 'white',
+// place: 'home',
+// numberOfPeople: 1,
+// enlightenment: 'off',
+// scent: 'Lavanda',
+// thePlaceWhereWork: 'table',
+// noise: 'on',
+// seat: 'chair',
+// numberOfSeats: 2,
+// room: 'living room'
+// }
+
+
+// let cat = {};
+// console.log(typeof cat) 
+// визначає вид 'typeof' (object, srting, massive)
+
+// let cat = {
+// 		'name': 'Tom',
+// 		'age': 3,
+// 		'color': 'Gray'
+
+// };
+
+// let dog = {
+// 		name: 'Rex',
+// 		color: 'black',
+// 		age: 4
+// };
+
+// console.log(cat); 
+// console.log(dog);
+
+// console.log(cat['name']);
+// console.log(cat.name);
+
+// console.log(Object.keys(cat)); 
+// перевірка ключів
+
+// let rabbit = {};
+
+// rabbit.name = 'Robert';
+// rabbit.color = 'white';
+// rabbit.age = 1;
+// console.log(rabbit);
+
+
+
+// let animals = [{
+// 		category: 'cats',
+// 		name: 'Tom',
+// 		age: 2,
+// 		color: 'gray'
+// 	},
+// 	{
+// 		category: 'dogs',
+// 		name: 'Rex',
+// 		age: 4,
+// 		color: 'black'
+// 	}
+// ];
+
+// animals[0].age = (3);
+
+// animals[0].age = animals[0].age+1;
+
+
+// console.log(animals[0].name);
+// console.log(animals[0].age);
+// console.log(animals[1].name);
+// console.log(animals[1].category);
+
